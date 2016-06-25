ccc0f812245e0f83f47e78e1f0fb333db08a1067
Arthur Lee <me@arthur.li>
2016-06-25 21:44:09 +0800

    Specify config file names in program arguments

--- app/cyborg.hs
+++ app/cyborg.hs
@@ -6,10 +6,15 @@ import Borg
 
 import qualified Data.Aeson as J
 import qualified Data.ByteString.Lazy as B
+import System.Environment (getArgs)
 
 main :: IO ()
 main = do
-  eJSON <- J.eitherDecode' <$> B.readFile "config.json"
-  case eJSON of
-    Left err -> fail err
-    Right rJSON -> checkConnectionThenBackup rJSON
+  filePaths <- getArgs
+  let  go :: FilePath -> IO ()
+       go jsonFilePath = do
+         eJSON <- J.eitherDecode' <$> B.readFile jsonFilePath
+         case eJSON of
+           Left err -> fail err
+           Right rJSON -> checkConnectionThenBackup rJSON
+  mapM_ go filePaths
